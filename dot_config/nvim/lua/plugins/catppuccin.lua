4c3b5cffaeb31afa3c929541d40f88e223095da0
Jason Lau <i@dotkrnl.com>
2024-06-28 15:55:53 -0700

    feat: add catppuccin for plugins

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -13,7 +13,38 @@ return {
 					dim_context = true,
 					alt_background = true,
 				},
+				cmp = true,
 				fidget = true,
+				leap = true,
+				markdown = true,
+				mason = true,
+				mini = { enabled = true },
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				neotree = true,
+				telescope = {
+					enabled = true,
+				},
+				treesitter = true,
+				treesitter_context = true,
 				which_key = true,
 			},
 		})
--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,15 @@
 return {
 	plugins = {
 		"nvim-treesitter/nvim-treesitter",
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 	setup = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = "all",
 			highlight = { enable = true },
+		})
+		require("treesitter-context").setup({
+			enable = true,
 		})
 	end,
 }

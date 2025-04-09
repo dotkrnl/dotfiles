00242a1cb7803f1d9903587a5b1ad93637ffcdfc
Jason Lau <i@dotkrnl.com>
2025-04-09 10:20:52 -0500

    chore: remove codecompanion and add buildifier

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -5,8 +5,9 @@ return {
 	setup = function()
 		require("conform").setup({
 			formatters_by_ft = {
-        c = { "clang-format" },
-        cpp = { "clang-format" },
+				bzl = { "buildifier" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
 				html = { "djlint" },
 				javascript = { { "prettierd", "prettier" } },
 				lua = { "stylua" },
